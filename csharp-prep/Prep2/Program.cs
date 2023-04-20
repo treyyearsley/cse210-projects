526a7988fdfc225babce4af9357c938f5d0e9011
treyyearsley <tdyyear@gmail.com>
2023-04-19 21:57:01 -0600

    C# prep code added

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+           letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You grade is: {letter}");
+
+         if (percent >= 70)
+         {
+            Console.WriteLine("Congratulations! You Passed!");
+         }
+         else
+         {
+            Console.WriteLine("Better luck next time!");
+         }
     }
 }
