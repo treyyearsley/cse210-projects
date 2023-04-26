da5b66e69e72dba44bcc15f9d67868b0d13c6b6c
treyyearsley <tdyyear@gmail.com>
2023-04-25 20:00:45 -0600

    Code added to Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2023;
+        job1._endYear = 2027;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Software Tester";
+        job2._startYear = 2027;
+        job2._endYear = 2029;
+
+        Resume myResume = new Resume();
+        myResume._name = "Trey Yearsley";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
