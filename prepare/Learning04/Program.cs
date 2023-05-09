bd29ac94aee00c287925ef077781d96e327964fc
treyyearsley <tdyyear@gmail.com>
2023-05-08 21:31:01 -0600

    coded added to learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        
+        Assignment assignment1 = new Assignment("Trey Yearsley", "Division");
+        Console.WriteLine(assignment1.GetSummary());
+
+        
+        MathAssignment assignment2 = new MathAssignment("Anna Yearsley", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Zander Yearsley", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
