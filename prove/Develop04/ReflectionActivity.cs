dc93e815a5c1d92725354414644b0324aae3e262
treyyearsley <tdyyear@gmail.com>
2023-05-11 19:10:27 -0600

    changes made to develop 04

--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -31,8 +31,7 @@ public class ReflectionActivity : Activity
     private string _description = "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.";
 
 
-    // Constructors
-    // Methods
+    
     public ReflectionActivity(string activityName, int activityTime) : base(activityName, activityTime)
     {
 
